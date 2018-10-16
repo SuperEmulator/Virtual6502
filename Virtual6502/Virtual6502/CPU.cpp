ed4e4e20f7d3d3ec08b152f8c5afd4e1e70d01a1
Shvier <daulvi@hotmail.com>
2018-10-16 20:11:50 +0800

    initial commit

---
+++ Virtual6502/Virtual6502/CPU.cpp
@@ -0,0 +1,9 @@
+//
+//  CPU.cpp
+//  Virtual6502
+//
+//  Created by Shvier on 2018/10/16.
+//  Copyright © 2018 SuperEmulator. All rights reserved.
+//
+
+#include "CPU.hpp"
---
+++ Virtual6502/Virtual6502/CPU.hpp
@@ -0,0 +1,14 @@
+//
+//  CPU.hpp
+//  Virtual6502
+//
+//  Created by Shvier on 2018/10/16.
+//  Copyright © 2018 SuperEmulator. All rights reserved.
+//
+
+#ifndef CPU_hpp
+#define CPU_hpp
+
+#include <stdio.h>
+
+#endif /* CPU_hpp */
