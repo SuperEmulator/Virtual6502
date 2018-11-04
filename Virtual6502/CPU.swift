480c6c623a6d31434f8decc4409bbc80b4b36772
Shvier <daulvi@hotmail.com>
2018-11-04 21:12:40 +0800

    add class CPU

---
+++ Virtual6502/CPU.swift
@@ -0,0 +1,13 @@
+//
+//  CPU.swift
+//  Virtual6502
+//
+//  Created by Shvier on 2018/11/4.
+//  Copyright © 2018 SuperEmulator. All rights reserved.
+//
+
+import Cocoa
+
+class CPU: NSObject {
+
+}
