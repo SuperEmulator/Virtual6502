2110099aa406b9e0e7f74710cbcb7a2d9691ed5d
Shvier <daulvi@hotmail.com>
2018-11-04 12:38:31 +0800

    use Swift instead of C++

---
+++ Virtual6502/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Virtual6502
+//
+//  Created by Shvier on 2018/11/4.
+//  Copyright © 2018 SuperEmulator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
